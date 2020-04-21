c417e5310211fa2fd9462a4b4d757cfd47af788d
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-21 15:18:54 +0100

    Add styling to overall app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import CardList from "./cards/CardList";
+import CardSearch from "./cards/CardSearch";
+import appStyle from "./App.css";
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <CardList />
+            <div className="background">
+                <div className="ui container">
+                    <CardSearch />
+                    <CardList />
+                </div>
             </div>
         );
     }
