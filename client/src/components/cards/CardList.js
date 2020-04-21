e1a1316b58220c1977bd192061ccdf7d29273e7a
Abdulqadir Qasim <tiamat@andromeda.local>
2020-04-21 10:21:11 +0100

    Retrieve all cards through 6 requests and render on screen

--- client/src/components/cards/CardList.js
+++ client/src/components/cards/CardList.js
@@ -19,20 +19,38 @@ class GamesList extends React.Component {
 
     componentDidMount = async () => {
         const token = await this.getAuthToken();
-        const response = await endpoint.get("/cards", {
-            headers: { Authorization: `Bearer ${token}` },
-        });
-        this.setState({ cards: response.data.cards });
+        for (let i = 1; i <= 6; i++) {
+            let response = await endpoint.get("/cards", {
+                headers: { Authorization: `Bearer ${token}` },
+                params: {
+                    page: i,
+                    pageSize: 500,
+                },
+            });
+            console.log(response);
+            this.setState({ cards: [...this.state.cards, ...response.data.cards] });
+        }
+        console.log(this.state.cards);
     };
 
     renderCards() {
-        const card_list = this.state.cards.map((card) => <div>{card.name.en_US}</div>);
+        const card_list = this.state.cards.map((card) => <img key={card.id} src={card.image.en_US}></img>);
         return card_list;
     }
 
     render() {
-        return <div>{this.renderCards()}</div>;
+        return <div className="ui small images">{this.renderCards()}</div>;
     }
 }
 
 export default GamesList;
+
+/*
+TODO:
+- there's 2548 cards currently
+- (seems like) max 500 cards per page, therefore 6 pages
+- not all cards have the same properties. e.g. spell cards don't have 'minionType' metadata
+- so, load all cards, match up card properties with 'metadata'
+- allow user to select radio buttons: class, minion, spell, mana etc. to filter by
+
+*/
